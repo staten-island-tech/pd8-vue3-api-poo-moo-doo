a602760ca8d219e8eb3c2e83642e6a70b06bd094
melinamui <113132371+melinamui@users.noreply.github.com>
2023-04-24 13:59:06 -0400

    Update Boy.vue

--- baby-names/src/views/Boy.vue
+++ baby-names/src/views/Boy.vue
@@ -1,3 +1,18 @@
-<script setup></script>
+<template>
+    <div>
 
-<template></template>
+    </div>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue'
+const babyNames = ref('')
+async function getBoys('https://data.cityofnewyork.us/resource/25th-nujf.json'){
+    let response
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
