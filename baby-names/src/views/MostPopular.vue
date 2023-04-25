384d7d802fb21ac44ed3ff9f0b300ce9351d78a6
DariaG16 <dariagolitsyna16@gmail.com>
2023-04-25 13:35:30 -0400

    Update MostPopular.vue

--- baby-names/src/views/MostPopular.vue
+++ baby-names/src/views/MostPopular.vue
@@ -3,11 +3,15 @@
 <script setup>
 import{ref, onMounted} from `vue`
 const babies = ref(``)
+
 async function getBabies(){
     let res = await fetch(`https://data.cityofnewyork.us/resource/25th-nujf.json`)
     let data = await res.json()
     babies.value = data.results
 }
+onMounted(()=>{
+    getBabies()
+})
 </script>
 
 <style scoped></style>
