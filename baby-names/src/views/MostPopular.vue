67b61771b08c635fc5bdf33b4343cf54cbe803fe
DariaG16 <dariagolitsyna16@gmail.com>
2023-04-25 13:47:33 -0400

    saving the project

--- baby-names/src/views/MostPopular.vue
+++ baby-names/src/views/MostPopular.vue
@@ -3,11 +3,11 @@
 <script setup>
 import{ref, onMounted} from `vue`
 const babies = ref(``)
-
 async function getBabies(){
     let res = await fetch(`https://data.cityofnewyork.us/resource/25th-nujf.json`)
     let data = await res.json()
     babies.value = data.results
+    console.log("hi")
 }
 onMounted(()=>{
     getBabies()
