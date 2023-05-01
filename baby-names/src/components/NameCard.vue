7fb366b0e39a3f2feffca9db787233ec2e0b3d1b
melinamui <113132371+melinamui@users.noreply.github.com>
2023-05-01 14:00:58 -0400

    Create NameCard.vue

---
+++ baby-names/src/components/NameCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <h2>{{ baby.nm }}</h2>
+    <h3>{{id}}</h3>
+  </div>
+</template>
+
+<script setup>
+const props = {
+  baby: Object,
+  id: Number
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
