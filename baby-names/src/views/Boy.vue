1f815c1f484fc8cdfbee22f9d3cb32b21a8c62af
melinamui <113132371+melinamui@users.noreply.github.com>
2023-05-01 19:01:04 -0400

    THE CHART WORKSSSSS

--- baby-names/src/views/Boy.vue
+++ baby-names/src/views/Boy.vue
@@ -1,16 +1,49 @@
 <template>
+  <div>
+    <canvas id="boysChart"></canvas>
+  </div>
   <h1>THE BOYS</h1>
 </template>
 
+
+
 <script setup>
+
+import Chart from 'chart.js/auto'
 import { ref, onMounted } from 'vue'
+
 const babyNames = ref('')
-const url = "https://data.cityofnewyork.us/resource/25th-nujf"
+
 async function getBoys() {
   let response = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
   let data = await response.json()
   babyNames.value = data
   console.log(data)
+
+  const ctx = document.getElementById('boysChart');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: data.map(row => row.nm),
+      datasets: [
+        {
+          label: 'Boy Names',
+          data: data.map(row => row.brth_yr),
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  }
+
+  )
+
 }
 onMounted(() => {
   getBoys()
