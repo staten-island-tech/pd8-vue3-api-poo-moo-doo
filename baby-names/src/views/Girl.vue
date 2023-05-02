dd7d59297da85e09f78775863f2457ffc28d9121
DariaG16 <dariagolitsyna16@gmail.com>
2023-05-02 13:37:10 -0400

    revert the revert?

--- baby-names/src/views/Girl.vue
+++ baby-names/src/views/Girl.vue
@@ -1,13 +1,58 @@
-<script setup></script>
-
 <template>
   <div id="graph">
     <canvas id="girlsChart"></canvas>
+<<<<<<< HEAD
     <h1>THE GALS</h1>
   </div>
+=======
+  </div>
+  <h1>THE GALS</h1>
+>>>>>>> parent of d88264c (Revert "hi we did it!!")
 </template>
 
-<style>
-background-color: rgb(221, 0, 203);
-background-blend-mode: luminosity;
+<script setup>
+import Chart from 'chart.js/auto'
+import { ref, onMounted } from 'vue'
+
+const babyNames = ref('')
+
+async function getBoys() {
+  let response = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
+  let data = await response.json()
+  babyNames.value = data
+  console.log(data)
+
+  const girls = data.filter((data) => data.gndr === 'FEMALE')
+
+  const ctx = document.getElementById('girlsChart')
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: girls.map((row) => row.nm),
+      datasets: [
+        {
+          label: 'Count of ',
+          data: girls.map((row) => row.cnt),
+          borderWidth: 2
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
+}
+onMounted(() => {
+  getBoys()
+})
+</script>
+
+<style scoped>
+#graph {
+  background-color: rgba(240, 248, 255, 0.555);
+}
 </style>
--- baby-names/src/views/MostPopular.vue
+++ baby-names/src/views/MostPopular.vue
@@ -3,16 +3,41 @@
 </template>
 
 <script setup>
+import Chart from 'chart.js/auto'
 import { ref, onMounted } from 'vue'
 
-const babies = ref('')
-async function getBabies() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
-  let data = await res.json()
-  babies.value = data
+const babyNames = ref('')
+
+async function getBoys() {
+  let response = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
+  let data = await response.json()
+  babyNames.value = data
+  console.log(data)
+
+  const ctx = document.getElementById('boysChart')
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: data.map((row) => row.nm),
+      datasets: [
+        {
+          label: 'Boy Names',
+          data: data.map((row) => row.brth_yr),
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
 }
 onMounted(() => {
-  getBabies()
+  getBoys()
 })
 </script>
 
