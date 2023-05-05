f2667c01cd186d151ddd0883e4e8d6cc03df8c67
DariaG16 <dariagolitsyna16@gmail.com>
2023-05-05 14:01:30 -0400

    fff

--- baby-names/src/App.vue
+++ baby-names/src/App.vue
@@ -6,7 +6,13 @@ import { RouterLink, RouterView } from 'vue-router'
   <router-link to="/" custom v-slot="{ navigate }">
     <button @click="navigate" role="link">back</button></router-link
   >
-
+  <router-link to="/girl" custom v-slot="{ navigate }"
+    >>
+    <label class="switch">
+      <input @click="navigate" role="link" type="checkbox" />
+      <span class="slider"> boy | girl</span>
+    </label>
+  </router-link>
   <RouterView />
 </template>
 
@@ -29,5 +35,53 @@ h1 {
   color: white;
   margin: 20px;
   padding: 20px;
+}
+.switch {
+  text-align: center;
+  position: absolute;
+  display: inline-block;
+  right: 40px;
+  margin: 50px;
+  width: 180px;
+  height: 76px;
+}
+
+.slider {
+  text-align: center;
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ffa9a9;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: '';
+  height: 68px;
+  width: 86px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+input:checked + .slider {
+  content: '';
+  background-color: #2196f3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(86px);
+  -ms-transform: translateX(86px);
+  transform: translateX(86px);
 }
 </style>
