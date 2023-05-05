2f4b1f6581f2581f90f47cc0d259789ee8495067
DariaG16 <dariagolitsyna16@gmail.com>
2023-05-05 13:48:33 -0400

    trying to change the width

--- baby-names/src/App.vue
+++ baby-names/src/App.vue
@@ -3,18 +3,24 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <button>hi!</button>
+  <router-link to="/" custom v-slot="{ navigate }">
+    <button @click="navigate" role="link">back</button></router-link
+  >
+
   <RouterView />
 </template>
 
 <style scoped>
 button {
-  background-color: rgb(102, 97, 253);
+  background-color: #f6e297;
   left: 100px;
   top: 60px;
-  width: 60px;
-  height: 30px;
+  width: 120px;
+  height: 60px;
   position: fixed;
+  font-size: 20px;
+  cursor: pointer;
+  border: solid 3px white;
 }
 h1 {
   background-color: rgba(250, 141, 159, 0.808);
