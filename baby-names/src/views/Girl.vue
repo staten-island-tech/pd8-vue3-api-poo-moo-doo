8f14cee16ed50ed069712a013bf1b70bd1bc5e7e
melinamui <113132371+melinamui@users.noreply.github.com>
2023-05-05 14:01:08 -0400

    got the charts for boy/most/girl (girl doesn't work)

--- baby-names/src/views/Girl.vue
+++ baby-names/src/views/Girl.vue
@@ -1,56 +1,46 @@
 <template>
-  <div id="graph">
-    <h1>THE GALS</h1>
-    <canvas id="girlsChart"></canvas>
+  <h1>Most Popular Girl Names</h1>
+  <div>
+    <Bar id="my-chart-id" v-if="loaded" :options="chartOptions" :data="chartData" />
   </div>
 </template>
 
-<script setup>
-import Chart from 'chart.js/auto'
-import { ref, onMounted } from 'vue'
-
-const babyNames = ref('')
-
-async function getGirls() {
-  let response = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
-  let data = await response.json()
-  babyNames.value = data
-  console.log(data)
-
-  const girls = data.filter((data) => data.gndr === 'FEMALE' && data.rnk < 11)
-  console.log(girls.sort())
-
-  const ctx = document.getElementById('girlsChart')
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: girls.map((row) => row.nm),
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+ChartJS.register(ArcElement, Tooltip, Legend)
+export default {
+  name: 'BabyChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: null,
+      chartOptions: null,
+      loaded: false
+    }
+  },
+  async mounted() {
+    const babyApi = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
+    const bbNames = await babyApi.json()
+    const namesArr = bbNames.filter((data) => data.gndr === 'FEMALE' && data.rnk < 11)
+    const names = namesArr.sort()
+    console.log(names)
+    this.chartData = {
+      labels: names.map((row) => row.nm),
       datasets: [
         {
-          label: '# of Times Used',
-          data: girls.map((row) => row.cnt),
+          label: 'Count',
+          data: names.map((row) => row.cnt),
           borderWidth: 1
         }
       ]
-    },
-    options: {
-      indexAxis: 'y',
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
     }
-  })
+    this.chartOptions = {
+      responsive: true
+    }
+    this.loaded = true
+  }
 }
-onMounted(() => {
-  getGirls()
-})
 </script>
 
-<style scoped>
-#graph {
-  background-color: rgba(240, 248, 255, 0.555);
-}
-</style>
+<style scoped></style>
